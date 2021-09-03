0c48a783e629ffa42616b81cedae51a0c60826b8
ny-a <58758248+ny-a@users.noreply.github.com>
2021-09-03 11:04:40 +0900

    Implement csv converter

--- test.py
+++ test.py
@@ -7,32 +7,35 @@ file = open("54.csv","r")
 out_file = open("output.csv","w")
 
 # 書き出し用ファイルのヘッダーを記述
-#out_file.write("lat,lng,control\n")
+out_file.write("lat,lng,control\n")
 
-# 元ファイルのヘッダーをreadlineメソッドで１行飛ばす
-for i in range(9):
-    file.readline()
-# 元ファイルのレコード部分をreadlinesメソッドで全行を読み取る
-lines = file.readlines()
+lat = 0.0
+lng = 0.0
+yaw = 0.0
 
-num = 0
 # for文で1行ずつ取得
-for line in lines:
-    if num % 3 == 0: #MPU9250のデータから、 MyYawを取得
-        # 改行コードをブランクに置換
-        line = line.replace("\n","")
-        # カンマ区切りでリストに変換する
-        line = line.split(",")
-        line = ','.join(line)
-    elif num % 3 == 1: #GPUから座標を取得
-        # 改行コードをブランクに置換
-        line = line.replace("\n","")
-        # カンマ区切りでリストに変換する
-        line = line.split(",")
-        line = ','.join(line)
-    out_file.write(line)
-    
-    num += 1
+for line in file.readlines():
+    values = line.strip().split(",")
+    if len(values) < 5:
+        # カラムが少なすぎる不要なStateとかLogは飛ばす
+        continue
+    if values[0] == "MPU9250":
+        if values[1] == "Yaw":
+            # ヘッダーは飛ばす
+            continue
+        yaw = values[1]
+    if values[0] == "GPS":
+        if values[1] == "Sats":
+            # ヘッダーは飛ばす
+            continue
+        lat = values[3]
+        lng = values[4]
+    if values[0] == "State":
+        if lat == 0.0 and lng == 0.0 and yaw == 0.0:
+            continue
+        # 制御ループの最後でStateを吐いているので、このタイミングで吐く
+        out_file.write(",".join([str(lat), str(lng), str(yaw)]) + "\n")
+
 
 # ２つのファイルを閉じる
 file.close()
